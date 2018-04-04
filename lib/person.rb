d85532d3b2b6d01283eecf2539e38bac7f521f2f
Dawn Catanach <dawn.catanach@gmail.com>
2018-04-04 22:18:13 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,11 @@
-class Person 
+class Person
 
-  def talk 
-    puts "Hello World!" 
-  end 
+  def talk
+    puts "Hello World!"
+  end
 
-  def walk 
+  def walk
     puts "The Perso is walking"
-  end 
+  end
 
-end 
+end
